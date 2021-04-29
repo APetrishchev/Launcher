682fdb2c2909cf58451ca9c0d8172f6d5f51ea23
apetrishchev <PetriAndPetro@gmail.com>
2021-04-29 14:48:26 +0600

    init

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,20 @@
 window.addEventListener("load", () => {
-  // if ("serviceWorker" in navigator) {
-  //   navigator.serviceWorker.register("scripts/sw.js")
-  //   .then(() => navigator.serviceWorker.ready.then((worker) => {
-  //     console.log("ServiceWorker registration successful with scope:", worker.scope)
-  //     worker.sync.register("syncdata")}))
-  //   .catch(
-  //     (err) => console.log("ServiceWorker registration failed:", err))
-  // }
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("scripts/sw.js")
+    .then(() => navigator.serviceWorker.ready.then((worker) => {
+      console.log("ServiceWorker registration successful with scope:", worker.scope)
+      worker.sync.register("syncdata")}))
+    .catch(
+      (err) => console.log("ServiceWorker registration failed:", err))
+  }
 
   // importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js")
-
-  workbox.setConfig({
-    debug: true
-  })
-  workbox.core.skipWaiting()
-  workbox.core.clientClaim()
-  workbox.precaching.precacheAndRoute([])
+  // workbox.setConfig({
+  //   debug: true
+  // })
+  // workbox.core.skipWaiting()
+  // workbox.core.clientClaim()
+  // workbox.precaching.precacheAndRoute([])
 
     // {
     //   "src": "/images/icons/apple-touch-icon.png",
