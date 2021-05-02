4be228eb07d64c95444b12da1e66ad8f59bdbd76
apetrishchev <PetriAndPetro@gmail.com>
2021-05-03 00:14:45 +0600

    init

--- scripts/main.js
+++ scripts/main.js
@@ -15,11 +15,12 @@ class Calendar {
   static getWeekdayNames() {
     let formatter = new Intl.DateTimeFormat("ru", {weekday: "short"})
     let fragment = document.createDocumentFragment()
-    for(let idx = 1; idx < 8; idx++) {
+    for (let idx = 1; idx < 8; idx++) {
       let innerHTML = formatter.format(new Date(2017, 4, idx))
       innerHTML = innerHTML[0].toUpperCase() + innerHTML.slice(1)
       let div = document.createElement("div")
-      div.className = idx < 6 ? "CalendarWeekday" : "CalendarWeekday CalendarWeekend"
+      let className = "TextScale CalendarWeekday"
+      div.className = idx < 6 ? className : `${className} CalendarWeekend`
       div.innerHTML = innerHTML
       fragment.appendChild(div)
     }
@@ -37,29 +38,77 @@ class Calendar {
     let now = new Date()
     let todayWeekday = now.getDay() || 7
     let daysInMonth = Calendar.getDaysInMonth(now.getFullYear(), now.getMonth() + 1)
-    console.log(now.getMonth(), daysInMonth)
     this.element = document.createElement("div")
     this.element.className = "CalendarPanel"
     this.element.appendChild(Calendar.getWeekdayNames())
     parent.appendChild(this.element)
-    let pos = 0
-    for(let row = 1; row < 7; row++) {
+    let pos = 1
+    for (let row = 1; row < 7; row++) {
       for (let col = 1; col < 8; col++) {
         let div = document.createElement("div")
         this.element.appendChild(div)
-        if (!pos && col < todayWeekday || pos >= daysInMonth) {
+        if (row == 1 && col < todayWeekday - 1 || pos >= daysInMonth) {
           div.className = "CalendarDay"
           continue
         }
-        div.className = col < 6 ? "CalendarDay" : "CalendarDay CalendarWeekend"
-        div.innerHTML = ++pos
+        let className = "CalendarDay"
+        if (col > 5) {
+          className += " CalendarWeekend"
+        }
+        if (pos == now.getDate()) {
+          className += " CalendarToday"
+        }
+        div.className = className
+        div.innerHTML = pos < 10 ? `0${pos}` : pos
+        pos++
       }
     }
   }
 }
 
 //******************************************************************************
+function onResize(args) {
+  console.log(this, args)
+  // this.resize()
+}
+
+//******************************************************************************
 function main() {
+  window._measuring_ = document.createElement("div")
+  window._measuring_.id = "measuring"
+  document.body.appendChild(window._measuring_)
+
+  window.addEventListener("resize", (evn) => {
+    for (const elm of document.querySelectorAll(".TextScale")) {
+      let fontSize = parseInt(window.getComputedStyle(elm).getPropertyValue('font-size'), 10)
+      window._measuring_.innerHTML = elm.innerHTML
+      window._measuring_.style.fontSize = `${fontSize}px`
+
+      let width = window._measuring_.clientWidth
+      // while (Math.round(width) != Math.round(elm.clientWidth)) {
+        if (width > elm.clientWidth) {
+          fontSize -= 1
+        }
+        if (width < elm.clientWidth) {
+          fontSize += 1
+        }
+        window._measuring_.style.fontSize = `${fontSize}px`
+        width = window._measuring_.clientWidth
+      // }
+      elm.style.fontSize = `${fontSize}px`
+      console.log(fontSize)
+
+      // while (!(fontSize - 1 < elm.clientHeight && elm.clientHeight < fontSize + 1)) {
+      //   if(elm.clientHeight > top) {
+      //     top *= 2
+      //   }
+      //   if(elm.clientHeight > fontSize) {
+      //     fontSize *= 2
+      //   }
+      // }
+    }
+  })
+
   let div = document.createElement("div")
   div.className = "Toolbar"
   div.innerHTML = "= = = = = = = = = ="
